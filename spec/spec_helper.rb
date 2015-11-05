5462aef074a8d2b3ba95a77daf2def9598d99bb6
Ilya Dolgirev <ilya.dolgirev@gmail.com>
2015-11-05 12:54:13 +0300

    Fix HoundCI code style violations

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -1,9 +1,8 @@
-ENV['RAILS_ENV'] ||= 'test'
+ENV["RAILS_ENV"] ||= "test"
 
-require File.expand_path("../dummy/config/environment.rb",  __FILE__)
-require 'rspec/rails'
-# require 'rspec/autorun'
-require 'factory_girl_rails'
+require File.expand_path("../dummy/config/environment.rb", __FILE__)
+require "rspec/rails"
+require "factory_girl_rails"
 
 Rails.backtrace_cleaner.remove_silencers!
 
@@ -11,7 +10,6 @@ Dir["#{File.dirname(__FILE__)}/support/**/*.rb"].each { |f| require f }
 
 RSpec.configure do |config|
   config.mock_with :rspec
-  # config.use_transactional_fixtures = true
   config.infer_base_class_for_anonymous_controllers = false
   config.order = "random"
 end
