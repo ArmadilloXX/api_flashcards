d4f3a6c0cbf0cc96f3b4381ac3ad07dc3044a641
Ilya Dolgirev <ilya.dolgirev@gmail.com>
2015-11-08 14:02:27 +0300

    Add cards#create route

--- config/routes.rb
+++ config/routes.rb
@@ -2,9 +2,10 @@ ApiFlashcards::Engine.routes.draw do
   get "main/welcome"
   root to: "main#welcome"
 
-    scope module: "api" do
-      namespace "v1" do
-        get "cards", to: "cards#index"
-      end
+  scope module: "api" do
+    namespace "v1" do
+      get "cards", to: "cards#index"
+      post "cards", to: "cards#create"
     end
+  end
 end
