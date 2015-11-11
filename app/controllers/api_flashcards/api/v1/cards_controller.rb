fb5d2f03af60031d8bad60cef666ba748c4aa9f5
Ilya Dolgirev <ilya.dolgirev@gmail.com>
2015-11-11 17:01:29 +0300

    Comment all tests(will be covered within main app). Cosmetic fixes in API parameters

--- app/controllers/api_flashcards/api/v1/cards_controller.rb
+++ app/controllers/api_flashcards/api/v1/cards_controller.rb
@@ -2,84 +2,79 @@ module ApiFlashcards
   class Api::V1::CardsController < ApiController
     include Swagger::Blocks
 
-    swagger_path '/cards' do
+    swagger_path "/cards" do
       operation :get do
-        key :description, 'Returns all user cards'
-        key :produces, [
-          'application/json'
-        ]
-        key :tags, [
-          'Cards'
-        ]
-        response 200 do
-          key :description, 'Response with array of cards'
-          # schema do
-          #   key :type, :array
-          #   items do
-          #     key :'$ref', :Card
-          #   end
-          # end
+        key :description, "Returns all user cards"
+        key :produces, ["application/json"]
+        key :tags, ["Cards"]
 
+        response 200 do
+          key :description, "Response with array of cards"
           schema do
-            key :'$ref', :CardsResponse
+            key :"$ref", :CardsResponse
           end
         end
+
         response 401 do
-          key :description, 'Not Authorized'
+          key :description, "Not Authorized"
           schema do
-            key :'$ref', :ErrorModel
+            key :"$ref", :NotAuthorized
           end
         end
       end
+
       operation :post do
-        key :description, 'Creates a new card.  Duplicates are allowed'
-        key :produces, [
-          'application/json'
-        ]
-        key :tags, [
-          'Cards'
-        ]
+        key :description, "Creates a new card.  Duplicates are allowed"
+        key :produces, ["application/json"]
+        key :tags, ["Cards"]
+
         parameter do
           key :name, "card[original_text]"
           key :in, "query"
-          key :description, 'Original text'
+          key :description, "Original text"
           key :required, true
           key :type, :string
         end
+
         parameter do
-          key :name, 'card[translated_text]'
+          key :name, "card[translated_text]"
           key :in, "query"
-          key :description, 'Translated text'
+          key :description, "Translated text"
           key :required, true
           key :type, :string
         end
+
         parameter do
-          key :name, 'card[block_id]'
+          key :name, "card[block_id]"
           key :in, "query"
-          key :description, 'Block for card'
+          key :description, "Block for card"
           key :required, true
           key :type, :integer
         end
+
         response 201 do
-          key :description, 'Response with card'
+          key :description, "Response with card"
           schema do
-            key :'$ref', :CardResponse
+            key :"$ref", :CardResponse
           end
         end
+
         response 401 do
-          key :description, 'Not Authorized'
+          key :description, "Not Authorized"
           schema do
-            key :'$ref', :ErrorModel
+            key :"$ref", :NotAuthorized
           end
         end
+
         response 422 do
-          key :description, 'Unprocessable entity'
+          key :description, "Unprocessable entity"
           schema do
-            key :'$ref', :ErrorModel
+            key :"$ref", :Unprocessable
           end
         end
       end
     end
+
     def index
       @cards = current_user.cards.all.order("review_date")
       render json: @cards, each_serializer: CardSerializer
@@ -90,7 +85,7 @@ module ApiFlashcards
       if @card.save
         render json: @card, serializer: CardSerializer, status: 201
       else
-        render json: { result: "Can't save your card", errors: "#{@card.errors.full_messages}" }, status: 422
+        render json: { result: "Can\'t save your card", errors: "#{@card.errors.full_messages}" }, status: 422
       end
     end
 
--- app/controllers/api_flashcards/main_controller.rb
+++ app/controllers/api_flashcards/main_controller.rb
@@ -2,33 +2,6 @@ require "swagger/blocks"
 module ApiFlashcards
   class MainController < ActionController::Base
     include Swagger::Blocks
-      swagger_root do
-        key :swagger, '2.0'
-        info do
-          key :version, '1.0.0'
-          key :title, 'Flashcards API'
-          key :description, 'A sample API for Flashcards App'
-        end
-        tag do
-          key :name, 'Cards'
-          key :description, 'Cards operations'
-        end
-        tag do
-          key :name, 'Trainer'
-          key :description, 'Training operations'
-        end
-        key :host, 'localhost:3000'
-        key :basePath, '/api/v1'
-        key :produces, ['application/json']
-      end
-
-      SWAGGERED_CLASSES = [
-        Api::V1::CardsController,
-        ::Card,
-        # ErrorModel,
-        self,
-      ].freeze
-
       def welcome
         render json: Swagger::Blocks.build_root_json(SWAGGERED_CLASSES)
       end
